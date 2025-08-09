0e422e200ce72e8adcb468ee8afaf14be8d8cedb
Nisrina Ghaniz <nisrina.ghaniz@gmail.com>
2025-08-09 15:19:14 +0700

    fix: user-course

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,6 +9,9 @@ router.use('/users', require('./user-routes'));
 // Course routes
 router.use('/courses', require('./course-routes'));
 
+// User-course routes
+router.use('/user_courses', require('./user-course-routes'));
+
 // Module routes (nested di dalam course)
 router.use('/courses/:courseSlug/modules', require('./module-routes'));
 
